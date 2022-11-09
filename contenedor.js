533a740fa1a1fc87c3ddf869ad8b3e3cb466a6c5
DiegoBandhy <diegoby1994@gmail.com>
2022-11-09 09:06:45 -0500

    falta deleteById

--- contenedor.js
+++ contenedor.js
@@ -1,110 +1,117 @@
-const fs = require('fs');
-// const { randomUUID } = require('crypto')
+const fs = require("fs");
 
-
-class FileContainer {
-    constructor(path) {
-        this.path = path;
+class Archivo {
+    constructor() {
+        this.filepath = './peliculas.txt'
     }
-    
-    
-    //Métodos
 
-    async save(object) {
-        let objects = await this.getAll();
-        
-        if (objects.some(o => o.id == object.id)) return;
-        
-        objects.push(object);
-        
+    async leer () {
+
         try {
-            fs.promises.writeFile(this.path, JSON.stringify(objects, null, 2));
-        
-        } catch (error) {
-            throw new Error(`Error en guardar objeto de id ${object.id}`);
+            const peliculas = await fs.promises.readFile(this.filepath, 'utf-8')
+            return JSON.parse(peliculas);
+        } catch (err) {
+            return [];
         }
-        return
- 
+  
     }
+
+    async borrar () {
+        await fs.promises.unlink(this.filepath);
+    }
+    
  
+    async guardar (titulo, precio, thumbnail) {
+        try {
+            const peliculas = await this.leer()
+            const nuevaPelicula = {
+                titulo,
+                precio,
+                thumbnail,
+                id: peliculas.length + 1,
+            };
+            peliculas.push(nuevaPelicula);
+            await fs.promises.writeFile(this.filepath, JSON.stringify(peliculas, null, 2));
+            return `Se ha agregado pelicula ${titulo}`;
+        } catch (err) {
+            console.log('error'. err);
+        }
+
+    }
+
     async getById(id) {
-        let objects = await this.getAll();
-        return objects.find(p => p.id == id) ?? null;
-    }
- 
-    async getAll() {
-        let objects;
+
         try {
-            objects = await fs.promises.readFile(this.path, 'utf-8');
+        
+        let peliculasGuardados = await fs.promises.readFile(this.filepath,"utf-8")
+        const arrayDePeliculas = JSON.parse(peliculasGuardados);
+        let peliFound = arrayDePeliculas.find((ele) =>
+                ele.id === id ? ele : undefined
+        
+        );
+        
+        console.log(peliFound);
         } catch (error) {
-            throw new Error(`Error en leer archivo ${this.path}`);
+        
+        console.log(error);
+            }
+        
         }
-        if (objects.length == 0) return [];
-        objects = [...JSON.parse(objects)];
-        return objects;
-    }
- 
-    async deleteById(id) {
-        let objects = await this.getAll();
-        let index = objects.findIndex(o => o.id == id);
-        if (index == -1) return;
-        objects.splice(index, index == 0 ? index + 1 : index);
-        try {
-            return fs.promises.writeFile(this.path, JSON.stringify(objects, null, 2));
-        } catch (error) {
-            throw new Error(`Error en escritura, al borrar objeto de id ${id}`);
-        }
-    }
- 
-    async deleteAll() {
-        try {
-            return fs.promises.writeFile(this.path, JSON.stringify([], null, 2));
-        } catch (error) {
-            throw new Error("Error en escritura, al borrar todos los objetos");
-        }
-    }
+
+        async deleteById(id) {
+            //await fs.promises.unlink(this.filepath)
+            try {
+            let peliculasCompletas = await fs.promises.writeFile(this.filepath,"utf-8")
+            
+            const peliculaConId = JSON.parse(peliculasCompletas);
+            
+            let peliculaSinId = peliculaConId.filter
+            (peliculas => peliculas.id <= 2 ) // no lee el id
+            //((nuevaPelicula) =>
+            //    nuevaPelicula.id != id ? ele : undefined)
+
+            //(peliculas => {
+            //    return peliculas.titulo != id;
+            //  })
+            //((peliculas) => peliculas.id !== Number)
+               // (peliculas => peliculas.id -2)
+              // persona.hobbies = persona.hobbies.filter(function (hobbie) {
+              //  return hobbie.name !== 'Photography';
+              //})
+                // (peli) =>
+            
+                // peli.id === id ? ele : undefined
+               // );
+            console.log(peliculaSinId);
+            
+            } catch (error) {
+            
+            console.log(error, "no funciona ");
+            
+            }
+            
+            }
+    
 }
- 
-async function main() {
-    let fc = new FileContainer("productos.txt");
-	
-
-    
-await fc.save({
-         title: 'Pelicula 1',
-         price: 190,
-         thumbnail: 'random',
-         id: 1
-     });
-     console.log(await fc.getAll());
- 
-   
-    await fc.save({
-        title: 'BB Terráqueo',
-        price: 345.67,
-        thumbnail: 'random',
-        id: 4
-    });
 
 
-    console.log(await fc.getAll());
- 
+const main = async () => {
+    const manejadorDeArchivos = new Archivo();
+   console.log("Leer: ", await manejadorDeArchivos.leer());
+   console.log(await manejadorDeArchivos.guardar("Argentina 1985", 100, "immagen.png"));
+  // console.log(await manejadorDeArchivos.guardar("Titanic", 200, "imagen.png"));
+  // console.log(await manejadorDeArchivos.guardar("Documental Mundial", 300, "poster.png"));
+  // console.log("Leer: ", await manejadorDeArchivos.leer());
+  // console.log( await manejadorDeArchivos.getById(2));
+   // console.log( await manejadorDeArchivos.deleteById(2));
+
+//     setTimeout( async () => {
+//         await manejadorDeArchivos.borrar();
+//     }, 5000);
     
-    // console.log(await fc.getById(4));
- 
-    
-    // console.log(await fc.getById(999));
- 
-   
-    
-    // console.log(await fc.deleteById(4));
-    // console.log(await fc.getAll());
- 
- 
-   
-    // await fc.deleteAll();
-    // console.log(await fc.getAll());
- 
+//     console.log("Leer: ", await manejadorDeArchivos.leer());
 }
+
+main();
 
 
